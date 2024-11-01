c6a0cf5f298e312f2b37ecc5cc54af0438bb201d
Manuel <manudelpino0@gmail.com>
2024-11-01 14:55:02 -0300

    Update API URL to use NEXT_PUBLIC environment variable; add backend start script

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 // src/api.ts
 
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
-console.log("Using API URL:", API_URL);
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
 
 // Types
 interface BotNamesResponse {
