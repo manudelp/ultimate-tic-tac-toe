89841980a737a987151496cedbbc590ab679d030
Manuel <manudelpino0@gmail.com>
2024-11-02 01:34:14 -0300

    Remove unused SVG files and update Open Graph metadata in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ const spaceGroteskBold = localFont({
 export const metadata: Metadata = {
   title: "Ultimate Tic Tac Toe",
   description: "Play Ultimate Tic Tac Toe!",
+  openGraph: {
+    images: [
+      {
+        url: "public/icon.png",
+        width: 800,
+        height: 600,
+        alt: "Ultimate Tic Tac Toe",
+      },
+    ],
+    siteName: "Ultimate Tic Tac Toe",
+    title: "Ultimate Tic Tac Toe",
+    description: "Play Ultimate Tic Tac Toe!",
+    url: "https://utictactoe.vercel.app",
+  },
 };
 
 export default function RootLayout({
