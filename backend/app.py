c10c0d3a3d41c0c87db36c711c0bb512bda56994
Manuel Delpino <manudelpino0@gmail.com>
2024-11-08 18:21:02 -0300

    fix: restrict CORS origins to the specified domain

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)  # Load configuration
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "https://www.utictactoe.online"}})
 
 # Initialize JWT
 jwt = JWTManager(app)
