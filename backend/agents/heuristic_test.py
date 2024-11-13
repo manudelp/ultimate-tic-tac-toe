7abc0dcbc105f4c2fde2d953fe2f9a402ca515a9
ManuM435 <mmeirino@udesa.edu.ar>
2024-11-13 13:10:29 -0300

    setting up the known locals

--- backend/agents/heuristic_test.py
+++ backend/agents/heuristic_test.py
@@ -43,3 +43,64 @@ IterVanBytesAgent = IterVanBytesAgent()
 JardineritoAntiMidAgent = JardineritoAntiMidAgent()
 BetterJardineritoAgent = BetterJardineritoAgent()
 BetterItterinoAgent = BetterItterinoAgent()
+
+# Test boards setup
+local_OneWon_1 = np.array([[1, 1, 1],
+                    [0, -1, -1],
+                    [0, 0, -1]])  # Player 1 wins on the top row
+
+local_OneWon_2 = np.array([[0, -1, 1],
+                    [0, 1, -1],
+                    [1, 0, -1]])  # Player 1 wins on the diagonal
+
+local_OneWon_3 = np.array([[-1, -1, 0],
+                    [1, 1, 1],
+                    [0, -1, 0]])  # Player 1 wins on the middle row
+
+local_TwoWon_1 = np.array([[0, -1, 1],
+                    [-1, -1, 1],
+                    [1, -1, 0]])  # Player -1 wins on the middle column
+
+local_TwoWon_2 = np.array([[-1, 1, 0],
+                    [1, -1, 0],
+                    [1, 0, -1]])  # Player -1 wins on the diagonal
+
+local_TwoWon_3 = np.array([[-1, -1, -1],
+                    [1, 1, 0],
+                    [0, 1, 0]])  # Player -1 wins on the top row
+
+local_BothWnbl_1 = np.array([[1, -1, -1],
+                    [1, -1, -1],
+                    [0, 1, 1]])  # No winner yet
+
+local_BothWnbl_2 = np.array([[1, 1, 0],
+                    [-1, -1, 0],
+                    [0, 0, 0]])  # Not a win yet, but close
+
+local_BothWnbl_3 = np.array([[0, 0, 0],
+                    -[1, 1, 0],
+                    -[-1, -1, 0]])  # Another close board without a winner
+
+local_BothWnbl_4 = np.array([[-1, -1, 0],
+                    [-1, 0, 1],
+                    [0, 1, 1]]) # winnable by 1 in (0, 2), (2, 0) || winnable by -1 in (0, 2), (2, 0)
+
+local_Draw_1 = np.array([[1, -1, 1],
+                    [1, -1, -1],
+                    [-1, 1, 1]])  # Draw (no more moves possible)
+
+local_toDraw_1 = np.array([[1, -1, 1],
+                    [-1, -1, 1],
+                    [0, 1, -1]])  # Secured Draw (will always be Draw)
+
+local_toDraw_2 = np.array([[-1, 0, 1],
+                    [1, -1, -1],
+                    [-1, 1, 1]]) # Secured Draw (will always be Draw)
+
+local_OneWnbl_1 = np.array([[1, 0, 1],
+                    [1, 0, 0],
+                    [0, 1, 1]]) # winnable by 1 in (0, 1), (1, 1), (1, 2), (2, 0)
+
+local_TwoWnbl_1 = np.array([[0, 0, 0],
+                    [-1, -1, 0],
+                    [-1, -1, 0]]) # winnable by -1 in (0, 0), (0, 1), (0, 2), (1, 2), (2, 2)
