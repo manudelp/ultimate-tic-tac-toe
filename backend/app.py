2382899291146c992a7b7f1b086c275f12b41ce5
Manuel Delpino <manudelpino0@gmail.com>
2024-11-14 02:14:36 -0300

    Update API URL to use specific IP address for backend connections

--- backend/app.py
+++ backend/app.py
@@ -55,4 +55,4 @@ def unhandled_exception(e):
 # Run the Flask app
 if __name__ == '__main__':
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='26.29.97.86', port=5000, debug=True)
