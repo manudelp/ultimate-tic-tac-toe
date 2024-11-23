d6dc812b31a4d51a8fd589559403eedcb9950d2b
ManuM435 <mmeirino@udesa.edu.ar>
2024-11-22 21:50:52 -0300

    refactor: rename evaluation method for clarity in BetterJardineritoAgent

--- backend/agents/bot/jarditobetter.py
+++ backend/agents/bot/jarditobetter.py
@@ -228,9 +228,9 @@ class BetterJardineritoAgent:
         """
 
         # BCheck Base Case
-        (ev_00, res_00), (ev_01, res_01), (ev_02, res_02) = self.get_eval_glob_hash(board[0, 0]), self.get_eval_glob_hash(board[0, 1]), self.get_eval_glob_hash(board[0, 2])
-        (ev_10, res_10), (ev_11, res_11), (ev_12, res_12) = self.get_eval_glob_hash(board[1, 0]), self.get_eval_glob_hash(board[1, 1]), self.get_eval_glob_hash(board[1, 2])
-        (ev_20, res_20), (ev_21, res_21), (ev_22, res_22) = self.get_eval_glob_hash(board[2, 0]), self.get_eval_glob_hash(board[2, 1]), self.get_eval_glob_hash(board[2, 2])
+        (ev_00, res_00), (ev_01, res_01), (ev_02, res_02) = self.get_board_info(board[0, 0]), self.get_board_info(board[0, 1]), self.get_board_info(board[0, 2])
+        (ev_10, res_10), (ev_11, res_11), (ev_12, res_12) = self.get_board_info(board[1, 0]), self.get_board_info(board[1, 1]), self.get_board_info(board[1, 2])
+        (ev_20, res_20), (ev_21, res_21), (ev_22, res_22) = self.get_board_info(board[2, 0]), self.get_board_info(board[2, 1]), self.get_board_info(board[2, 2])
         results_board = np.array([[res_00, res_01, res_02], [res_10, res_11, res_12], [res_20, res_21, res_22]])
         winner = self.get_winning_result_hash(results_board)
         if winner != 0:
