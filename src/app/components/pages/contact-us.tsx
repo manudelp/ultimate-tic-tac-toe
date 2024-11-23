35942cb8f96e94106406b60f63705347a9a2e5a1
Manuel Delpino <manudelpino0@gmail.com>
2024-11-23 16:21:41 -0300

    no light mode

--- src/app/components/pages/contact-us.tsx
+++ src/app/components/pages/contact-us.tsx
@@ -15,18 +15,18 @@ const ContactUs: React.FC = () => {
 
   return (
     <div className="contact-us min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-8 bg-white shadow-md dark:bg-gray-800">
-        <h2 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-gray-200">
+      <div className="w-full max-w-md p-8 shadow-md bg-gray-800">
+        <h2 className="text-2xl font-semibold mb-2 text-gray-200">
           Contact Us
         </h2>
-        <h3 className="text-lg font-medium mb-6 text-gray-600 dark:text-gray-400">
+        <h3 className="text-lg font-medium mb-6 text-gray-400">
           We are here to assist you with any inquiries
         </h3>
         <form>
           <div className="mb-4">
             <label
               htmlFor="name"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              className="block text-sm font-medium text-gray-300"
             >
               Name:
             </label>
@@ -36,13 +36,13 @@ const ContactUs: React.FC = () => {
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-600 bg-gray-700 text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              className="block text-sm font-medium text-gray-300"
             >
               Email:
             </label>
@@ -52,13 +52,13 @@ const ContactUs: React.FC = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-600 bg-gray-700 text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
           <div className="mb-4">
             <label
               htmlFor="message"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              className="block text-sm font-medium text-gray-300"
             >
               Message:
             </label>
@@ -67,12 +67,12 @@ const ContactUs: React.FC = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-600 bg-gray-700 text-gray-200 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
           <a
             href={generateMailToLink()}
-            className="block w-full text-center py-2 px-4 bg-indigo-600 text-white font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-indigo-500 dark:hover:bg-indigo-600"
+            className="block w-full text-center py-2 px-4 text-white font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 bg-indigo-500 hover:bg-indigo-600"
           >
             Submit
           </a>
