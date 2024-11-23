b76faf3e70e358369b5b7f56a69f67599aea6dd3
Manuel Delpino <manudelpino0@gmail.com>
2024-11-22 21:30:48 -0300

    refactor: simplify header component by removing drag-and-drop functionality and hardcoding position

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,64 +1,11 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const positions = {
-  topLeft: "top-0 left-0",
-  topRight: "top-0 right-0",
-  bottomLeft: "bottom-0 left-0",
-  bottomRight: "bottom-0 right-0",
-};
-
 const Header = () => {
-  const [position, setPosition] = useState<keyof typeof positions>(() => {
-    const savedPosition = localStorage.getItem("headerPosition");
-    return (savedPosition as keyof typeof positions) || "bottomRight";
-  });
-
-  const handleDragEnd = (e: { clientX: number; clientY: number }) => {
-    const { clientX, clientY } = e;
-    const { innerWidth, innerHeight } = window;
-
-    let newPosition: keyof typeof positions;
-    if (clientX < innerWidth / 2 && clientY < innerHeight / 2) {
-      newPosition = "topLeft";
-    } else if (clientX >= innerWidth / 2 && clientY < innerHeight / 2) {
-      newPosition = "topRight";
-    } else if (clientX < innerWidth / 2 && clientY >= innerHeight / 2) {
-      newPosition = "bottomLeft";
-    } else {
-      newPosition = "bottomRight";
-    }
-
-    setPosition(newPosition);
-    localStorage.setItem("headerPosition", newPosition);
-  };
-
-  useEffect(() => {
-    const headerElement = document.querySelector(".absolute.m-4");
-    if (
-      headerElement &&
-      (
-        performance.getEntriesByType(
-          "navigation"
-        )[0] as PerformanceNavigationTiming
-      ).type === "navigate"
-    ) {
-      headerElement.classList.add("animate-bounce");
-      const timeout = setTimeout(() => {
-        headerElement.classList.remove("animate-bounce");
-      }, 500);
-      return () => clearTimeout(timeout);
-    }
-  }, [position]);
-
   return (
-    <div
-      className={`absolute m-4 ${positions[position]}`}
-      draggable
-      onDragEnd={handleDragEnd}
-    >
+    <div className="absolute bottom-0 left-0 m-4" draggable>
       <Link to="/" reloadDocument>
-        <div className="p-4 bg-gray-950 rounded-full">
+        <div className="p-4 bg-gray-950 bg-opacity-50 border border-gray-950 backdrop-blur rounded-full">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
