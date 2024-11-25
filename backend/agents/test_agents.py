35080d883a09aa486e04a82205764f56228b0bfd
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-11-25 20:23:54 -0300

    added blizzard mode to test agents

--- backend/agents/test_agents.py
+++ backend/agents/test_agents.py
@@ -28,12 +28,18 @@ AGENT1 = JardineritoAgent()    # Replace with your chosen agent
 AGENT2 = BetterJardineritoAgent()  # Replace with your chosen agent
 ROUNDS = 1
 GAMES = ROUNDS * 2
+BLIZZARD_MODE = False
 
 agent1_name = str(AGENT1)
 agent2_name = str(AGENT2)
 
 # Run the simulation
-agent1_wins, agent2_wins, draws, agent1_time, agent2_time = utils.play_multiple_games(AGENT1, AGENT2, ROUNDS)
+if BLIZZARD_MODE:
+    print(Style.BRIGHT + Fore.LIGHTCYAN_EX + f"\n ----+---- 🌨️❄️ PLAYING {GAMES} GAMES IN BLIZZARD MODE ❄️🌨️ ----+----")
+    agent1_wins, agent2_wins, draws, agent1_time, agent2_time = utils.play_multiple_games(AGENT1, AGENT2, ROUNDS, gamemode="blizzard")
+else:
+    agent1_wins, agent2_wins, draws, agent1_time, agent2_time = utils.play_multiple_games(AGENT1, AGENT2, ROUNDS)
+
 if agent1_wins > agent2_wins:
     final_winner = agent1_name
 elif agent1_wins < agent2_wins:
