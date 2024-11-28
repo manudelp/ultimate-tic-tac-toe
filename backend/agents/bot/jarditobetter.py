0c70c5a538ad0fe34fde4c3dd5cf0a4ae5edf2ca
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-11-28 17:55:05 -0300

    new betterjardi icon

--- backend/agents/bot/jarditobetter.py
+++ backend/agents/bot/jarditobetter.py
@@ -15,7 +15,7 @@ class BetterJardineritoAgent:
     def __init__(self):
         self.id = 12
         self.name = "Don Jardito"
-        self.icon = ""
+        self.icon = "🚀"
         self.moveNumber = 0
         self.depth_local = 8 # when btp is not None
         self.depth_global = 7 # when btp is None
