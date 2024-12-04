f2fe36f2d9f3b40dec5918384cc31be4fc7a40ba
ManuM435 <mmeirino@udesa.edu.ar>
2024-12-04 20:39:46 -0300

    refactoring bots folder

--- tests/Timers/timer_basic.py
+++ tests/Timers/timer_basic.py
@@ -29,51 +29,58 @@ def lineEval(line, player=1):
         else:
             return 0
 
-def A(coord):
-    return coord in [(0, 0), (0, 2), (2, 0), (2, 2)]
+list_random = [20, 291, 9, 3829, 291, 29, 38, 11, 6]
 
-def B(x, y):
-    return (x+y) % 2 == 1
+def A(lista):
+    a0, a1, a2, a3, a4, a5, a6, a7, a8 = lista
+    return None
 
-def C(coord):
-    ''' Center '''
-    return coord == (1, 1)
+def B(lista):
+    a0 = lista[0]
+    a1 = lista[1]
+    a2 = lista[2]
+    a3 = lista[3]
+    a4 = lista[4]
+    a5 = lista[5]
+    a6 = lista[6]
+    a7 = lista[7]
+    a8 = lista[8]
+    return None
 
 # Timeit tests with direct lambda calls
-iters = 750_000
-samples = 2750
+iters = 32_000
+samples = 1600
 total_iters = iters * samples # 2_062_500_000
 total_time_A = 0
 total_time_B = 0
-total_time_C = 0
 
 for i in range(samples):
-    coord = random.randint(0, 2), random.randint(0, 2)
-    x, y = coord
+    time_A = timeit.timeit(lambda: A(list_random), number=iters)
+    time_B = timeit.timeit(lambda: B(list_random), number=iters)
 
-    if i % 250 == 0:
-        if A(coord):
-            state = "a corner"
-        elif B(x, y):
-            state = "an edge"
-        elif C(coord):
-            state = "a center"
-        print(f"At Iteration {i}, {coord} is {state} position")
-
-    time_A = timeit.timeit(lambda: A(coord), number=iters)
-    time_B = timeit.timeit(lambda: B(x, y), number=iters)
-    time_C = timeit.timeit(lambda: C(coord), number=iters)
-
-    if A(coord) == B(x, y) == C(coord):
-        raise ValueError(f"They were equal! Their values are: A({A(coord)}), B({B(x, y)}), C({C(coord)})! Coord was {coord}")
+    if A(list_random) != B(list_random):
+        raise ValueError(f"They were not equal! Their values are: A({A(list_random)}), B({B(list_random)})! List was {list_random}")
+    
+    if i%250 == 0:
+        percentage_completed = (i/samples) * 100
+        print(f"Completed {percentage_completed:.2f}% of the total samples...")
 
     total_time_A += time_A
     total_time_B += time_B
-    total_time_C += time_C
 
 # Print the time results
 print(f"Below are the Time Results after {total_iters} total iterations")
 print(f"Time for Option A: {total_time_A:.4f} seconds")
 print(f"Time for Option B: {total_time_B:.4f} seconds")
-print(f"Time for Option C: {total_time_C:.4f} seconds")
 
+time_difference = (total_time_A - total_time_B) / total_time_A
+min_diff = 0.05
+
+if time_difference < min_diff:
+    time_percentage_slower_B = ((total_time_B - total_time_A) / total_time_B) * 100
+    print(f"Option A was faster! Option B was slower by {time_percentage_slower_B:.2f}%")
+elif time_difference > min_diff:
+    time_percentage_slower_A = ((total_time_A - total_time_B) / total_time_A) * 100
+    print(f"Option B was faster! Option A was slower by {time_percentage_slower_A:.2f}%")
+else:
+    print("Both options took around the same")
