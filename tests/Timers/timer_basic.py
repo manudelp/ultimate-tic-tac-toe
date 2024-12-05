1f0fb86250c2eca952f5011f86a735adc9276f31
ManuM435 <mmeirino@udesa.edu.ar>
2024-12-05 17:47:59 -0300

    time approved manual tuple dot product

--- tests/Timers/timer_basic.py
+++ tests/Timers/timer_basic.py
@@ -29,58 +29,63 @@ def lineEval(line, player=1):
         else:
             return 0
 
-list_random = [20, 291, 9, 3829, 291, 29, 38, 11, 6]
-
-def A(lista):
+def A(lista, coefs):
     a0, a1, a2, a3, a4, a5, a6, a7, a8 = lista
-    return None
+    b0, b1, b2, b3, b4, b5, b6, b7, b8 = coefs
+    result = a0*b0 + a1*b1 + a2*b2 + a3*b3 + a4*b4 + a5*b5 + a6*b6 + a7*b7 + a8*b8
+    return result
 
-def B(lista):
-    a0 = lista[0]
-    a1 = lista[1]
-    a2 = lista[2]
-    a3 = lista[3]
-    a4 = lista[4]
-    a5 = lista[5]
-    a6 = lista[6]
-    a7 = lista[7]
-    a8 = lista[8]
-    return None
+def B(lista, coefs):
+    a_arr = np.array(lista)
+    b_arr = np.array(coefs)
+    result = np.dot(a_arr, b_arr)
+    return result
+
+# def C(lista, coefs):
+#     a_list = lista
+#     b_list = coefs
+#     result = sum([a*b for a,b in zip(a_list, b_list)])
+#     return result
+
+# def D(lista, coefs):
+#     a_list = lista
+#     b_list = coefs
+#     result = a_list @ b_list
+#     return result
 
 # Timeit tests with direct lambda calls
-iters = 32_000
-samples = 1600
+iters = 10_000
+samples = 1200
 total_iters = iters * samples # 2_062_500_000
 total_time_A = 0
 total_time_B = 0
+total_time_C = 0
+total_time_D = 0
 
 for i in range(samples):
-    time_A = timeit.timeit(lambda: A(list_random), number=iters)
-    time_B = timeit.timeit(lambda: B(list_random), number=iters)
-
-    if A(list_random) != B(list_random):
-        raise ValueError(f"They were not equal! Their values are: A({A(list_random)}), B({B(list_random)})! List was {list_random}")
+    a_list_rnd = random.sample(range(1, 100), 9)
+    b_list_rnd = random.sample(range(1, 100), 9)
     
-    if i%250 == 0:
+    time_A = timeit.timeit(lambda: A(a_list_rnd, b_list_rnd), number=iters)
+    time_B = timeit.timeit(lambda: B(a_list_rnd, b_list_rnd), number=iters)
+    # time_C = timeit.timeit(lambda: C(a_list_rnd, b_list_rnd), number=iters)
+    # time_D = timeit.timeit(lambda: D(a_list_rnd, b_list_rnd), number=iters)
+
+    # if not (A(a_list_rnd, b_list_rnd) == B(a_list_rnd, b_list_rnd) == C(a_list_rnd, b_list_rnd) == D(a_list_rnd, b_list_rnd)):
+    #     raise ValueError(f"Results are not the same! Results are {A(a_list_rnd, b_list_rnd)}, {B(a_list_rnd, b_list_rnd)}, {C(a_list_rnd, b_list_rnd)}, {D(a_list_rnd, b_list_rnd)} for parameters {a_list_rnd, b_list_rnd}")
+    
+    if i%200 == 0:
         percentage_completed = (i/samples) * 100
         print(f"Completed {percentage_completed:.2f}% of the total samples...")
 
     total_time_A += time_A
     total_time_B += time_B
+    # total_time_C += time_C
+    # total_time_D += time_D
 
 # Print the time results
 print(f"Below are the Time Results after {total_iters} total iterations")
 print(f"Time for Option A: {total_time_A:.4f} seconds")
 print(f"Time for Option B: {total_time_B:.4f} seconds")
-
-time_difference = (total_time_A - total_time_B) / total_time_A
-min_diff = 0.05
-
-if time_difference < min_diff:
-    time_percentage_slower_B = ((total_time_B - total_time_A) / total_time_B) * 100
-    print(f"Option A was faster! Option B was slower by {time_percentage_slower_B:.2f}%")
-elif time_difference > min_diff:
-    time_percentage_slower_A = ((total_time_A - total_time_B) / total_time_A) * 100
-    print(f"Option B was faster! Option A was slower by {time_percentage_slower_A:.2f}%")
-else:
-    print("Both options took around the same")
+# print(f"Time for Option C: {total_time_C:.4f} seconds")
+# print(f"Time for Option D: {total_time_D:.4f} seconds")
